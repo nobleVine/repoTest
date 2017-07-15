4c828c6cf9c7aa99f7d7811c931a8b091d4c69bd
lore893 <lore-maro-93@hotmail.it>
2017-07-15 11:11:16 +0200

    Test su input errati e su controllo tipo parametri (manca prezzo)

--- negozio/src/test/java/com/lorenzo/marco/prodotti/ProdottoTest.java
+++ negozio/src/test/java/com/lorenzo/marco/prodotti/ProdottoTest.java
@@ -26,6 +26,39 @@ public class ProdottoTest {
 		assertEquals("Maglietta basket", prodotto.getDescrizione());
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testInputNegativo() {
+		prodotto = creazioneProdotto("Maglietta", -9, "Maglietta basket");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNomeNonVuoto() {
+		prodotto = creazioneProdotto(null, 5, "Maglietta basket");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDescrizioneNonVuoto() {
+		prodotto = creazioneProdotto("Maglietta", 5, "");
+	}
+
+	@Test
+	public void testTipoNome() {
+		prodotto = creazioneProdotto("Maglietta", 1, "Maglietta basket");
+		assertTrue(prodotto.getNome() instanceof String);
+	}
+
+	/*
+	 * @Test public void testTipoPrezzo() { prodotto =
+	 * creazioneProdotto("Maglietta", 1, "Maglietta basket");
+	 * assertTrue(prodotto.getPrezzo() ); }
+	 */
+
+	@Test
+	public void testTipoDescrizione() {
+		prodotto = creazioneProdotto("Maglietta", 1, "Maglietta basket");
+		assertTrue(prodotto.getDescrizione() instanceof String);
+	}
+
 	private Prodotto creazioneProdotto(String nome, double prezzo, String descrizione) {
 		return new Prodotto(nome, prezzo, descrizione);
 	}
